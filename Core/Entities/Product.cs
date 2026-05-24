8056c6dea9d8a274835b12cf5bff9a2bf9005769
PouyaBapiry <pouyabapiry83@gmail.com>
2026-05-24 10:56:25 +0330

    Add project files.

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    [Table("MyProducts",Schema ="Base")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(120),Required]
+        public string ProductName { get; set; }
+        public long Price { get; set; }
+    }
+}
